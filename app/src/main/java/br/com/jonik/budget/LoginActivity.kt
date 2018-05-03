5dd243913af8d2066f7ad1f9a9dd49e458d282ed
arthurcordova <arthur.stapassoli@gmail.com>
2018-05-03 08:39:06 -0300

    Activity for new budget - Steps

--- app/src/main/java/br/com/jonik/budget/LoginActivity.kt
+++ app/src/main/java/br/com/jonik/budget/LoginActivity.kt
@@ -1,16 +1,30 @@
 package br.com.jonik.budget
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import br.com.jonik.jonikbudget.R
+import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        buttonSignIn.setOnClickListener(this)
 
     }
+
+    override fun onClick(p0: View?) {
+        when (p0?.id) {
+            R.id.buttonSignIn -> {
+                startActivity(Intent(this, NewBudgetActivity::class.java))
+                finish()
+            }
+        }
+    }
+
 
 }
---
+++ app/src/main/java/br/com/jonik/budget/NewBudgetActivity.kt
@@ -0,0 +1,164 @@
+package br.com.jonik.budget
+
+import android.support.design.widget.TabLayout
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+
+import br.com.jonik.jonikbudget.R
+import kotlinx.android.synthetic.main.activity_new_budget.*
+import kotlinx.android.synthetic.main.fragment_new_budget.view.*
+
+class NewBudgetActivity : AppCompatActivity() {
+
+    /**
+     * The [android.support.v4.view.PagerAdapter] that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * [android.support.v4.app.FragmentStatePagerAdapter].
+     */
+    private var mSectionsPagerAdapter: SectionsPagerAdapter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_budget)
+
+        setSupportActionBar(toolbar)
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
+
+        // Set up the ViewPager with the sections adapter.
+        container.adapter = mSectionsPagerAdapter
+
+        configureTabs()
+        container.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
+        tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(container))
+
+
+    }
+
+    private fun configureTabs() {
+        val itemsText = listOf<String>("1 Dados inicias", "2 Teste")
+
+        for (i in itemsText) {
+            tabs.addTab(tabs.newTab().setText(i))
+        }
+//        tabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+//            override fun onTabReselected(tab: TabLayout.Tab?) {}
+//
+//            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+//
+//            override fun onTabSelected(tab: TabLayout.Tab?) {
+////                tabFinancing.visibility = View.GONE
+////                tabVehicle.visibility = View.GONE
+////                tabPs.visibility = View.GONE
+////                tabPendencies.visibility = View.GONE
+////
+////                when (tab?.position) {
+////                    0 -> {
+////                        tabFinancing.visibility = View.VISIBLE
+////                    }
+////
+////                    1 -> {
+////                        tabVehicle.visibility = View.VISIBLE
+////                    }
+////
+////                    2 -> {
+////                        tabPs.visibility = View.VISIBLE
+////                    }
+////
+////                    3 -> {
+////                        tabPendencies.visibility = View.VISIBLE
+////                    }
+////                }
+//            }
+//
+//        })
+
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_new_budget, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+        if (id == R.id.action_settings) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    /**
+     * A [FragmentPagerAdapter] that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    inner class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+        override fun getItem(position: Int): Fragment {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1)
+        }
+
+        override fun getCount(): Int {
+            // Show 3 total pages.
+            return 2
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    class PlaceholderFragment : Fragment() {
+
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                                  savedInstanceState: Bundle?): View? {
+            val rootView = inflater.inflate(R.layout.fragment_new_budget, container, false)
+            rootView.section_label.text = getString(R.string.section_format, arguments?.getInt(ARG_SECTION_NUMBER))
+            return rootView
+        }
+
+        companion object {
+            /**
+             * The fragment argument representing the section number for this
+             * fragment.
+             */
+            private val ARG_SECTION_NUMBER = "section_number"
+
+            /**
+             * Returns a new instance of this fragment for the given section
+             * number.
+             */
+            fun newInstance(sectionNumber: Int): PlaceholderFragment {
+                val fragment = PlaceholderFragment()
+                val args = Bundle()
+                args.putInt(ARG_SECTION_NUMBER, sectionNumber)
+                fragment.arguments = args
+                return fragment
+            }
+        }
+    }
+}
